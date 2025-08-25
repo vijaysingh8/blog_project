22484a79807a0230cfe42c9b52424cb3fb84ab46
vijaysingh8 <148944738+vijaysingh8@users.noreply.github.com>
2025-08-25 21:30:05 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,7 +12,8 @@ export const register=async(req,res)=>{
         }
         const hashedPassword=await bcrypt.hash(password,10);
         const user=await User.create({name,email,password:hashedPassword,image:image_filename});
-        res.status(201).json({message:"Sign up successfully",success:true,user});
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
+        res.status(201).json({message:"Sign up successfully",success:true,user,token});
     } catch (error) {
         res.status(500).json({message:"Internal server error"});
     }
