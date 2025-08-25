ea5da4e48a0d7766feffb8c969ac53fe0ff36d59
vijaysingh8 <148944738+vijaysingh8@users.noreply.github.com>
2025-08-25 21:34:34 +0530

    Update SingleBlog.jsx

--- frontend/src/pages/SingleBlog.jsx
+++ frontend/src/pages/SingleBlog.jsx
@@ -8,13 +8,13 @@ const SingleBlog = () => {
   const blog=blogData.find((b)=>b._id===id);
   return (
     <div className='rounded-md border-1 border-gray-200 p-5 max-w-3xl flex-col gap-3 items-center justify-center mx-auto py-8'>
-    <img className='transition-transform duration-300 hover:scale-105' src={`http://localhost:4000/images/${blog.image}`} />
+    <img className='transition-transform duration-300 hover:scale-105' src={`${import.meta.env.VITE_BACKEND_URL}/images/${blog.image}`} />
     <p className='text-2xl font-bold'>{blog.title}</p>
     <p className='text-[#4B6BFB]'>{blog.category}</p>
     <p >{blog.description}</p>
     <div className='flex gap-2 items-center justify-center'>
       <p className='text-lg font-bold'>Author:{blog.author.name}</p>
-      <img className='w-8 h-8 rounded-full' src={`http://localhost:4000/images/${blog.author.image}`} alt="" />
+      <img className='w-8 h-8 rounded-full' src={`${import.meta.env.VITE_BACKEND_URL}/images/${blog.author.image}`} alt="" />
     </div>
     <p >
       {
