9ac93e88fbab08f1527027f6d63c2f27ae21cfd8
vijaysingh8 <148944738+vijaysingh8@users.noreply.github.com>
2025-08-25 21:31:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,24 @@ dotenv.config();
 const app=express();
 //middlewares
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 connectDB();
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (
+        !origin ||
+        origin.endsWith(".vercel.app") ||
+        origin === "http://localhost:5173"
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.get('/',(req,res)=>{
   res.send('hello world');
 });
